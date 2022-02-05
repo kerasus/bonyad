457ed8a177b5820072c52edc95928585ce7130ed
kerasus <aliesmaeeli20@gmail.com>
2022-02-05 16:41:35 +0330

    login update

--- pages/Login.vue
+++ pages/Login.vue
@@ -47,7 +47,7 @@
                 </v-col>
               </v-row>
             </v-toolbar-title>
-            <v-spacer />
+            <v-spacer/>
             <v-tooltip bottom>
               <template v-slot:activator="{ on }">
                 <v-btn
@@ -95,7 +95,7 @@
               >
                 ورود
               </v-btn>
-              <v-spacer />
+              <v-spacer/>
             </v-card-actions>
           </v-form>
         </v-card>
@@ -106,94 +106,104 @@
 
 <script>
 
-    import API_ADDRESS from '@/assets/Addresses'
-    import {User} from '@/models/User'
-    import { mixinAuth } from '@/mixin/Mixins'
+import API_ADDRESS from '@/assets/Addresses'
+import {User} from '@/models/User'
+import {mixinAuth} from '@/mixin/Mixins'
 
-    export default {
-        name: 'Login',
-        mixins: [mixinAuth],
-        data () {
-          return {
-            // user: new User(window.localStorage.getItem('user')),
-            loadingList: false,
-            username: null,
-            password: null,
-            autofilledUsername:false,
-            autofilledPass:false
-          }
-        },
-        head() {
-          return {
-            title: 'ورود'
-          }
-        },
-        created() {
-            // if (this.getToken()) {
-            //   this.getUserData( () => { this.redirectTo() })
-            // }
-        },
-      methods: {
-        getToken () {
-          return this.$store.getters['Auth/accessToken']
-        },
-        setUserData (userData) {
-          this.$store.commit('Auth/updateUser', new User(userData))
-        },
-        setAccessToken (access_token) {
-          this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + access_token
-          this.$store.commit('Auth/updateAccessToken', access_token)
-        },
-        redirectTo () {
-          // if (this.$route.query.redirect_to_exam) {
-          //   this.$router.push({path: '/user/create'})
-          //   return
-          // }
-          //
-          // let redirect_to = window.localStorage.getItem('redirect_to')
-          // if (!redirect_to) {
-          //   redirect_to = 'dashboard'
-          // }
-          // this.$router.push({ name: redirect_to })
-          // this.$router.push({path: '/user/create'})
-
-        },
-        login () {
-          let that = this
-          this.loadingList = true
-          this.$axios.post(API_ADDRESS.auth.login, {
-            mobile: this.username,
-            password: this.password
-          })
-            .then((response) => {
-
-              this.loadingList = false
-
-              that.user = new User(response.data.data.user)
-              that.$store.commit('Auth/updateUser', that.user)
-              const access_token = response.data.data.access_token
-              this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + access_token
-              this.setAccessToken(access_token)
-              that.setUserData(response.data.data.user)
-              that.$axios.get(API_ADDRESS.user.info).then((resp) => {
-                this.$store.commit('Auth/updateUser', resp.data.data)
-                this.$router.push({path: '/'})
-              }).catch(() => {
-                this.$store.dispatch('Auth/logout')
-                this.$notify({
-                  type: 'error',
-                  title: 'توجه',
-                  text: 'شما دسترسی لازم برای ورود به پنل را ندارید'
-                })
-              })
-              // this.getUserData(() => { this.redirectTo() })
-            })
-            .catch( () => {
-              this.loadingList = false
-            })
-        }
-      }
+export default {
+  name: 'Login',
+  mixins: [mixinAuth],
+  data() {
+    return {
+      // user: new User(window.localStorage.getItem('user')),
+      loadingList: false,
+      username: null,
+      password: null,
+      autofilledUsername: false,
+      autofilledPass: false
     }
+  },
+  head() {
+    return {
+      title: 'ورود'
+    }
+  },
+  computed: {
+    user() {
+      return this.$store.getters['Auth/user']
+    }
+  },
+  created() {
+    // if (this.getToken()) {
+    //   this.getUserData( () => { this.redirectTo() })
+    // }
+  },
+  methods: {
+    getToken() {
+      return this.$store.getters['Auth/accessToken']
+    },
+    setUserData(userData) {
+      this.$store.commit('Auth/updateUser', new User(userData))
+    },
+    setAccessToken(access_token) {
+      this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + access_token
+      this.$store.commit('Auth/updateAccessToken', access_token)
+    },
+    redirectTo() {
+      // if (this.$route.query.redirect_to_exam) {
+      //   this.$router.push({path: '/user/create'})
+      //   return
+      // }
+      //
+      // let redirect_to = window.localStorage.getItem('redirect_to')
+      // if (!redirect_to) {
+      //   redirect_to = 'dashboard'
+      // }
+      // this.$router.push({ name: redirect_to })
+      // this.$router.push({path: '/user/create'})
+
+    },
+    login() {
+      let that = this
+      this.loadingList = true
+      this.$axios.post(API_ADDRESS.auth.login, {
+        mobile: this.username,
+        password: this.password
+      })
+        .then((response) => {
+
+          this.loadingList = false
+
+          that.user = new User(response.data.data.user)
+          that.$store.commit('Auth/updateUser', that.user)
+          const access_token = response.data.data.access_token
+          this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + access_token
+          this.setAccessToken(access_token)
+          that.setUserData(response.data.data.user)
+          that.$axios.get(API_ADDRESS.user.info).then((resp) => {
+            this.$store.commit('Auth/updateUser', resp.data.data)
+            if (this.user.hasPermission('accessBonyadEhsanAdminPanel')) {
+              this.$router.push({path: '/'})
+            } else {
+              this.$router.push({path: '/abrisham/UserAbrishamProgress'})
+            }
+
+          }).catch(() => {
+            this.$store.dispatch('Auth/logout')
+            this.$notify({
+              type: 'error',
+              title: 'توجه',
+              text: 'شما دسترسی لازم برای ورود به پنل را ندارید'
+            })
+          })
+          // this.getUserData(() => { this.redirectTo() })
+        })
+        .catch(() => {
+          this.loadingList = false
+        })
+    }
+  }
+}
 </script>
 
 <style scoped>
