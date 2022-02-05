e1220bc4e0c6e58457266e2ed40d29364264c227
kerasus <aliesmaeeli20@gmail.com>
2022-02-05 16:14:48 +0330

    axios errors $notify

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,6 @@
 // https://nuxtjs.org/docs/directory-structure/middleware/#named-middleware
 export default function auth({store, redirect}) {
   let token = store.getters['Auth/accessToken']
-  console.log('token', token)
-  console.log('redirect', redirect)
   if (!token) {
     return redirect('/login')
   }
--- middleware/redirectAdmin.js
+++ middleware/redirectAdmin.js
@@ -1,6 +1,5 @@
 export default function redirectAdmin({store, redirect}) {
   let user = store.getters['Auth/user']
-  console.log('user', user)
   if (user.permissions && !user.permissions.find(item => item === 'accessBonyadEhsanPanel')) {
     return redirect('/')
   }
