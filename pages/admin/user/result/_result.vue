706ba7bc556373eb3247938adfeaf648f479de4f
kerasus <aliesmaeeli20@gmail.com>
2022-09-24 16:28:24 +0330

    - update result page chart - update result page based on major

--- pages/admin/user/result/_result.vue
+++ pages/admin/user/result/_result.vue
@@ -1,34 +1,69 @@
 <template>
-<div>
-  <highcharts :options="chartOptions" class="mb-10" />
-  <v-card
-    class="mx-auto"
-    color="#ff8f00"
-    dark
-    max-width="400"
-  >
-    <v-card-text class="text-h5 font-weight-bold text-center mb-10">
-      میانگین تراز کل : {{ averageRank }}
-    </v-card-text>
-  </v-card>
-  <v-data-table
-    :headers="headers"
-    :items="rows"
-    :items-per-page="5"
-    class="elevation-1"
-  />
-</div>
+  <div>
+    <v-select
+      v-model="selectedMajor"
+      :items="majors"
+      item-text="title"
+      item-value="value"
+      label="رشته"
+      @change="onChangeMajor"
+    />
+    <highcharts :options="chartOptions" class="mb-10"/>
+    <v-card
+      class="mx-auto"
+      color="#ff8f00"
+      dark
+      max-width="400"
+    >
+      <v-card-text class="text-h5 font-weight-bold text-center mb-10">
+        میانگین تراز کل : {{ averageRank }}
+      </v-card-text>
+    </v-card>
+    <v-data-table
+      :headers="headers"
+      :items="rows"
+      :items-per-page="5"
+      class="elevation-1"
+    />
+  </div>
 </template>
 
 <script>
-import { Chart } from 'highcharts-vue'
+import {Chart} from 'highcharts-vue'
 import API_ADDRESS from "assets/Addresses";
 
 export default {
   name: "moshaverResult",
-  components: { highcharts: Chart },
-  data () {
+  components: {highcharts: Chart},
+  data() {
     return {
+      selectedMajor: 1,
+      majors: [
+        {
+          title: 'ریاضی',
+          value: 1
+        },
+        {
+          title: 'تجربی',
+          value: 2
+        },
+        {
+          title: 'انسانی',
+          value: 3
+        },
+        {
+          title: 'علوم و معارف اسلامی',
+          value: 4
+        },
+        {
+          title: 'زبان',
+          value: 5
+        },
+        {
+          title: 'هنر',
+          value: 6
+        },
+      ],
       chartOptions: {
         chart: {
           height: 700,
@@ -96,83 +131,76 @@ export default {
           align: 'center',
           value: 'title',
         },
-        { text: 'عربی', value: 'lessons.arabi' },
-        { text: 'فارسی', value: 'lessons.farsi' },
-        { text: 'ریاضی', value: 'lessons.riazi' },
-        { text: 'شیمی', value: 'lessons.shimi' }
+        {text: 'عربی', value: 'lessons.arabi'},
+        {text: 'فارسی', value: 'lessons.farsi'},
+        {text: 'ریاضی', value: 'lessons.riazi'},
+        {text: 'شیمی', value: 'lessons.shimi'}
       ],
       rows: [],
     }
   },
   created() {
-    this.getStatistics().then(() => {
-      this.setCartData()
-    })
+    this.getStatistics()
+      .then(() => {
+        this.setCartData()
+      })
   },
-  computed:{
-    user () {
+  computed: {
+    user() {
       return this.$store.getters['Auth/user']
     }
   },
   methods: {
-    getUserOfBonyadId () {
+    onChangeMajor () {
+      this.getStatistics()
+        .then(() => {
+          this.setCartData()
+        })
+    },
+    getUserOfBonyadId() {
       return this.$route.params.result ? this.$route.params.result : this.user.id
     },
-    setCartData () {
+    setCartData() {
       this.setRankChart()
       this.setUserTable()
       this.setAverageRank()
     },
-    loadxAxis (data) {
+    loadxAxis(data) {
       this.chartOptions.xAxis.categories = data
     },
-    series (data) {
+    series(data) {
       this.chartOptions.series = data
     },
-    setRankChart () {
+    setRankChart() {
       this.setRankChartSeries()
       this.setRankChartAxis()
     },
-    setRankChartSeries(){
-      let config = [
-        {
-          name: 'تراز دروس اختصاصی',
-          data: []
-        },
-        {
-          name: 'تراز دروس عمومی',
-          data: []
-        },
-        {
-          name: 'میانگین تراز کل',
-          data: []
+    setRankChartSeries() {
+      this.series(this.flatData.rankChart.map(item=>{
+        return {
+          name: item.title,
+          data: item.data
         }
-      ]
-      this.flatData.rankChart.forEach(item => {
-        config[0].data.push(Math.round(item.proficiency))
-        config[1].data.push(Math.round(item.general))
-        config[2].data.push(Math.round(item.averageRank))
-      })
-      this.series(config)
+      }))
     },
-    setRankChartAxis(){
+    setRankChartAxis() {
       let config = []
       this.flatData.rankChart.forEach(item => {
         config.push(item.title)
       })
       this.loadxAxis(config)
     },
-    setUserTable () {
+    setUserTable() {
       this.rows = this.flatData.userTable
     },
-    setAverageRank () {
+    setAverageRank() {
       this.averageRank = this.flatData.averageRank
     },
-    async getStatistics () {
-      const [rankChart , userTable , averageRank] = await Promise.all([
-          this.getRankChart(),
-          this.getUserTable(),
-          this.getAverageRank()
+    async getStatistics() {
+      const [rankChart, userTable, averageRank] = await Promise.all([
+        this.getRankChart(),
+        this.getUserTable(),
+        this.getAverageRank()
       ])
       this.flatData = {
         rankChart: rankChart.data.data,
@@ -180,17 +208,17 @@ export default {
         averageRank: averageRank.data.data
       }
     },
-    getRankChart () {
+    getRankChart() {
       const id = this.getUserOfBonyadId()
-      return this.$axios.get(API_ADDRESS.exam.getRankChart(id))
+      return this.$axios.get(API_ADDRESS.exam.getRankChart(id, this.selectedMajor))
     },
-    getUserTable () {
+    getUserTable() {
       const id = this.getUserOfBonyadId()
-      return this.$axios.get(API_ADDRESS.exam.getUserRank(id))
+      return this.$axios.get(API_ADDRESS.exam.getUserRank(id, this.selectedMajor))
     },
-    getAverageRank () {
+    getAverageRank() {
       const id = this.getUserOfBonyadId()
-      return this.$axios.get(API_ADDRESS.exam.getAverageRank(id))
+      return this.$axios.get(API_ADDRESS.exam.getAverageRank(id, this.selectedMajor))
     }
   }
 }
