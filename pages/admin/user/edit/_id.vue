927599a8f56afeadcd95b13ef709bacf090b1fe4
kerasus <aliesmaeeli20@gmail.com>
2022-10-03 16:00:55 +0330

    add edit user page

---
+++ pages/admin/user/edit/_id.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="row">
+    <div class="col-md-3">
+      <v-text-field
+        label="نام"
+      />
+    </div>
+    <div class="col-md-3">
+      <v-text-field
+        label="نام خانوادگی"
+      />
+    </div>
+    <div class="col-md-3">
+      <v-select
+        :items="items"
+        label="Standard"
+      ></v-select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "_id.vue",
+  data () {
+    return {
+      user: {
+        major:
+      },
+      majors: [
+
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
